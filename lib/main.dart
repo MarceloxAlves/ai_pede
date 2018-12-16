210b013355eb2277f6ec6243230d1b84e6d3cf4b
Marcelo <marceloalvessoft@gmail.com>
2018-12-16 14:09:42 -0300

    AINDA ACHO QUE FUNCIONA

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:ai_pede/screens/home_screen.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
