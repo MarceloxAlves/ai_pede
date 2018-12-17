bfb31ee685a8da09d72c0b68c8b437e4a521d835
danielmessi13 <danielmessi13@hotmail.com>
2018-12-16 23:03:48 -0300

    Login, Bugs

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:ai_pede/screens/home_screen.dart';
+import 'package:ai_pede/screens/login_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
@@ -35,12 +36,12 @@ _teste() async {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    _teste();
+//    _teste();
     return MaterialApp(
       title: 'Ai Pede',
       debugShowCheckedModeBanner: false,
       theme: themeDefault,
-      home: HomeSreen(),
+      home: LoginPage(),
     );
   }
 }
---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,139 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart ';
+import 'package:ai_pede/tiles/produtogrid_tile.dart';
+import 'package:ai_pede/tiles/produtolist_tile.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => new _LoginPageState();
+}
+
+enum FormType{
+  login,
+  registrar
+}
+
+
+class _LoginPageState extends State<LoginPage> {
+  final formKey = new GlobalKey<FormState>();
+
+  String _email;
+  String _password;
+  FormType _formType = FormType.login;
+
+  bool validar() {
+    final form = formKey.currentState;
+    if (form.validate()) {
+      form.save();
+      return true;
+    }
+
+    return false;
+  }
+
+  Future submit() async {
+    if (validar()) {
+      try {
+        if (_formType == FormType.login){
+            FirebaseUser user = await FirebaseAuth.instance
+                .signInWithEmailAndPassword(email: _email, password: _password);
+            print('entrou');
+        }else{
+          FirebaseUser user = await FirebaseAuth.instance
+              .createUserWithEmailAndPassword(email: _email, password: _password);
+          print('criou');
+        }
+
+      } catch (e) {
+        Scaffold.of(formKey.currentContext).showSnackBar(SnackBar(content: Text('$e')));
+      }
+    }
+  }
+
+  void moveToRegister() {
+    formKey.currentState.reset();
+    setState(() {
+      _formType = FormType.registrar;
+    });
+  }
+
+  void moveToLogin() {
+    formKey.currentState.reset();
+    setState(() {
+      _formType = FormType.login;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login'),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(16.0),
+        child: Form(
+          key: formKey,
+            child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: buildInputs() + submitButtons()
+        )),
+      ),
+    );
+  }
+
+  List<Widget> buildInputs(){
+    return [
+      TextFormField(
+        decoration: new InputDecoration(labelText: 'Email'),
+        validator: (value) =>
+        value.isEmpty ? 'Email não pode estar  vazio!' : null,
+        onSaved: (value) => _email = value,
+      ),
+      TextFormField(
+        decoration: new InputDecoration(labelText: 'Password'),
+        validator: (value) =>
+        value.isEmpty ? 'Senha não pode estar vazio!' : null,
+        obscureText: true,
+        onSaved: (value) => _password = value,
+      ),
+    ];
+  }
+
+  List<Widget> submitButtons(){
+    if (_formType == FormType.login){
+      return [
+        RaisedButton(
+            padding: EdgeInsets.all(16.0),
+            child: new Text(
+              'Login',
+              style: TextStyle(fontSize: 20),
+            ),
+            onPressed: submit),
+        FlatButton(
+            onPressed: moveToRegister,
+            padding: EdgeInsets.all(8),
+            child: Text('Crie sua conta', style: TextStyle(fontSize: 20))),
+
+      ];
+
+    }else{
+      return [
+        RaisedButton(
+            padding: EdgeInsets.all(16.0),
+            child: new Text(
+              'Criar conta ',
+              style: TextStyle(fontSize: 20),
+            ),
+            onPressed: submit),
+        FlatButton(
+            onPressed: moveToLogin,
+            padding: EdgeInsets.all(8),
+            child: Text('Tem uma conta? Login', style: TextStyle(fontSize: 20)))
+      ];
+    }
+
+  }
+
+}
