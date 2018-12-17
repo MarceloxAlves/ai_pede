9caa119d94e76a65d88ddeccadd5c3d318e4112f
danielmessi13 <danielmessi13@hotmail.com>
2018-12-16 23:39:05 -0300

    Login, Bugs

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
-import 'package:ai_pede/screens/home_screen.dart';
 import 'package:ai_pede/screens/login_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,27 +8,8 @@ void main() {
 final ThemeData themeDefault = ThemeData(
     primarySwatch: Colors.deepOrange, accentColor: Colors.orangeAccent);
 
-final googleSignIn = GoogleSignIn();
-final auth = FirebaseAuth.instance;
-
-Future<Null> _login() async {
-  GoogleSignInAccount user = googleSignIn.currentUser;
-  if (user == null) user = await googleSignIn.signInSilently();
-  if (user == null) user = await googleSignIn.signIn();
-  if (await auth.currentUser() == null) {
-    GoogleSignInAuthentication credecials =
-        await user.authentication;
-    await auth.signInWithGoogle(
-        idToken: credecials.idToken, accessToken: credecials.accessToken);
-  }
-  assert(user == null);
-}
 
 //AIzaSyBCt4rwnHmE_nQ-FliUURvGuyaDB6ZWESQ
-
-_teste() async {
-  await _login();
-}
 
 class MyApp extends StatelessWidget {
   @override
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart ';
-import 'package:google_sign_in/google_sign_in.dart';
 
 class LoginPage extends StatefulWidget {
   @override
